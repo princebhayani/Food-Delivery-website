d4d9986e06c9d4e610c2e704a80d9e45c2869b34
princebhayani1902 <princebhayani192003@gmail.com>
2024-06-17 21:44:27 +0530

    set routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,19 @@
+import { Route, Routes } from "react-router-dom"
 import Navbar from "./components/Navbar/Navbar"
+import Home from "./pages/Home/Home"
+import Cart from "./pages/Cart/Cart"
+import PlaceOrder from "./pages/PlaceOrder/PlaceOrder"
 
 function App() {
 
   return (
     <div className="app">
       <Navbar />
+      <Routes >
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="placeorder" element={<PlaceOrder />} />
+      </Routes>
     </div>
   )
 }
