30e480f3360458ec034ee7270ddcd979fa31f858
princebhayani1902 <princebhayani192003@gmail.com>
2024-06-19 21:46:59 +0530

    add explore menu component

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import "./Home.css";
 import Header from '../../components/Header/Header';
+import ExploreMenu from '../../components/ExploreMenu/ExploreMenu';
 
 function Home() {
     return (
         <div>
             <Header />
+            <ExploreMenu />
             Home
         </div>
     )
