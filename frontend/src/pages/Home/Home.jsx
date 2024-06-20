41c1f8c83a92f9ca3cf5d01e7bfe4483c1cadde6
princebhayani1902 <princebhayani192003@gmail.com>
2024-06-20 22:05:10 +0530

    add set category functionality to explore menu list

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Home.css";
 import Header from '../../components/Header/Header';
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu';
 
 function Home() {
+    const [category, setCategory] = useState("All");
+    
     return (
         <div>
             <Header />
-            <ExploreMenu />
+            <ExploreMenu category={category} setCategory={setCategory}/>
             Home
         </div>
     )
